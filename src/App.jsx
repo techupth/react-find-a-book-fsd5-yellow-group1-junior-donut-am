a0fac9a88207443f9b0da325e57183f5c9421167
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2023-12-26 15:56:59 +0700

    feat:find a book

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [listBook, setListBook] = useState([]);
+  const [findBook, setFindBook] = useState("");
+
+  useEffect(() => {
+    findNameBooks();
+  }, [findBook]);
+
+  const findNameBooks = async() => {
+    try {
+      const result = await axios.get(
+        "https://www.googleapis.com/books/v1/volumes?q=" + findBook
+      );
+        setListBook(result.data.items);
+      } catch (error) {
+        setListBook([]);
+    } 
+  }
+  return (
+    <div className="App">
+      <h1 className="findABook">Find a Book</h1>
+      {status}
+      <input
+        type="text"
+        value={findBook}
+        onChange={(event) => {
+          setFindBook(event.target.value);
+        }}
+      />
+      <ul>
+        {listBook.map((books, index) => {
+          return <li key={index}>{books.volumeInfo.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
